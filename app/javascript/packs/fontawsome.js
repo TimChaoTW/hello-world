edade2ad5a8bd85c7bbb2e031854bfa651da96d7
Tim Chaio <timmy1993jaw@gmail.com>
2024-02-07 07:46:32 +0800

    add fontawsome and validates for title and date

---
+++ app/javascript/packs/fontawsome.js
@@ -0,0 +1,5 @@
+import { library, icon } from "@fortawesome/fontawesome-svg-core";
+import { faBook } from "@fortawesome/free-solid-svg-icons/faBook";
+
+library.add(faBook);
+dom.watch();
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+# Validate for title and date
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :publish_date, presence: true
 end
