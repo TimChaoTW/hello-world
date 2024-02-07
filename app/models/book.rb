6aebcff7a007c940974ca4efb25142970d7717e8
Tim Chaio <timmy1993jaw@gmail.com>
2024-02-07 08:34:18 +0800

    add image upload

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,10 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :publish_date, presence: true
+
+  has_one_attached :avatar
+
+  def book_image(size)
+    avatar.variant(resize_to_limit: [size, size]).processed
+  end
 end
